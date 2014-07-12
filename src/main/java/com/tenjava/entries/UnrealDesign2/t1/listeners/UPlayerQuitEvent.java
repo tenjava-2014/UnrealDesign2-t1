4e1d85aa2f52e38fa9e00b8492ba49eb57e49f91
Wil <wil@fragzone.org>
2014-07-11 20:24:41 -0500

    Added login/logout listeners

---
+++ src/main/java/com/tenjava/entries/UnrealDesign2/t1/listeners/UPlayerQuitEvent.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tenjava.entries.UnrealDesign2.t1.listeners;
+
+import com.tenjava.entries.UnrealDesign2.t1.players.UPlayerManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ *
+ * @author Wil
+ */
+public class UPlayerQuitEvent implements Listener
+{
+    public UPlayerQuitEvent()
+    {
+        
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e)
+    {
+        UPlayerManager manager = UPlayerManager.getInstance();
+        Player p = e.getPlayer();
+        
+        manager.logout(p);
+    }
+}
